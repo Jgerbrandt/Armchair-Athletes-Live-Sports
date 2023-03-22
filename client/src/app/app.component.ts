485fc6f592a97c4d1666169a0428c4cf3d038d31
AvijotGirn <avijot.girn@ucalgary.ca>
2023-03-22 12:55:23 -0600

    Styling stuff. homepage, navbar, logo

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,23 +4,37 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container-md" style="padding:1rem;
-  background: rgb(163,163,172);
-  background: radial-gradient(circle, rgba(163,163,172,1) 0%, rgba(86,87,95,1) 93%);
-  height:100hw;width:100vw;min-height:100vh;min-width:100vw">
   
+  background: rgb(233, 248, 249);
+  /*background: radial-gradient(circle, rgba(163,163,172,1) 0%, rgba(86,87,95,1) 93%);*/
+  height:100hw;width:100vw;min-height:100vh;min-width:100vw"
+  >
+
+  <div style="display: flex; 
+  flex-direction: row;">
+  <img src="/assets/media/logo.png"
+  style="
+    height:5rem;
+    width:5rem;
+    padding-bottom: 5px;
+  ">
+  
+
   <button [routerLink]="['home/']"
   style="
-  display:flex;
+  display: block;
+  width: 624px;
+  margin: 0 auto;
   justify-content: center;
-  margin-left: 25%;
   background: transparent;
-  border:none;
+  border: none;
+  font-family : Impact;
   ">
-  <h1>
-  
-  Armchair Athletes Live Sports Results</h1></button>
-  
+
+  <h1> Armchair Athletes Live Sports Results</h1></button>
+  </div>
     <router-outlet></router-outlet>
+    
   </div>
   `
 })
