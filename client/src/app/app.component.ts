2ffd7e6809610e5d5078b649bf89949eed789cfc
AvijotGirn <avijot.girn@ucalgary.ca>
2023-03-23 15:43:24 -0600

    recommit files for pr

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: '/app-global/app-global-component.html',
-    styleUrls: ['/app-global/app-global-component.css']
+  templateUrl: './app-global/app-global-component.html',
+    styleUrls: ['./app-global/app-global-component.css']
 })
 export class AppComponent { }
